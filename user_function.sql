8868fb16f3efcb2d07b5af437eae18b0f5164fb8
quinntd2506 <trinhdiemquynh2506@gmail.com>
2024-01-03 03:10:07 +0700

    Update user_function.sql

--- user_function.sql
+++ user_function.sql
@@ -128,40 +128,6 @@ END;
 $$ LANGUAGE plpgsql;
 --SELECT add_item_to_cart(69, 5, 'user3@gmail.com'); 
 
---DROP FUNCTION IF EXISTS remove_item_from_cart(int, character varying)
--- Function to remove an item from cart
-CREATE OR REPLACE FUNCTION remove_item_from_cart(in_product_id INT, in_email VARCHAR(150))
-RETURNS INTEGER AS $$
-DECLARE
-    in_user_id INT;
-    in_cart_id INT;
-BEGIN
-    SELECT user_id INTO in_user_id FROM account WHERE email = in_email;
-    SELECT cart_id INTO in_cart_id FROM cart WHERE user_id = in_user_id;
-
-    IF in_cart_id IS NOT NULL AND in_product_id IS NOT NULL THEN
-        -- Remove one item from cart_item
-        UPDATE cart_item
-        SET quantity = GREATEST(quantity - 1, 0)
-        WHERE cart_id = in_cart_id AND product_id = in_product_id;
-
-        -- Check if the quantity became zero, and delete the entry if so
-        IF (
-            SELECT quantity
-            FROM cart_item
-            WHERE cart_id = in_cart_id AND product_id = in_product_id
-        ) = 0 THEN
-            DELETE FROM cart_item
-            WHERE cart_id = in_cart_id AND product_id = in_product_id;
-        END IF;
-
-        RETURN 1; -- Success
-    ELSE
-        RETURN 0; -- Failed (User or product not found)
-    END IF;
-END;
-$$ LANGUAGE plpgsql;
---SELECT remove_item_from_cart(211, 'user3@gmail.com'); 
 
 -- Function to update the quantity of an item in cart
 CREATE OR REPLACE FUNCTION update_cart_item_quantity(in_cart_id INT, in_product_id INT, in_new_quantity INT)
