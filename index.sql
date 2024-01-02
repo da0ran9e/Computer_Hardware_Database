d839902048387e29f272762e253c7c3f1879f1c6
quinntd2506 <trinhdiemquynh2506@gmail.com>
2024-01-02 23:48:03 +0700

    Update index.sql

--- index.sql
+++ index.sql
@@ -14,10 +14,6 @@ CREATE INDEX idx_cart_item_cart_product ON cart_item(cart_id, product_id);
 CREATE INDEX idx_order_item_order_product ON order_item(order_id, product_id);
 --DROP INDEX IF EXISTS idx_order_item_order_product;
 
--- Improve performance of item-related functions because category_id is in a lot of WHERE clause
-CREATE INDEX idx_item_category_id ON item(category_id);
---DROP INDEX IF EXISTS idx_item_category_id;
-
 -- Improve performance of add/remove_one_to_inventory
 CREATE INDEX idx_inventory_warehouse_product ON inventory(warehouse_id, product_id);
 --DROP INDEX IF EXISTS idx_inventory_warehouse_product;
